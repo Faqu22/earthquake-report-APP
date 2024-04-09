62f9d3bde9a10817fd4428c42c4f17f306e19fa4
Faqu22 <facundoalvarez415@gmail.com>
2024-04-09 14:20:15 -0300

    Add Dockerfiles and docker-compose.yml for backend services

---
+++ backend/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Iniciando el contenedor del backend..."
+
+docker-compose up -d web
+
+while [[ "$(docker inspect -f '{{json .State.Running}}' backend_frogmi)" != "true" ]]; do
+    echo "Esperando a que el contenedor del backend inicie completamente..."
+    sleep 3
+done
+
+echo "Iniciando el contenedor de tareas..."
+
+docker-compose up -d cron
+
+echo "¡Todos los contenedores han sido iniciados correctamente!"
