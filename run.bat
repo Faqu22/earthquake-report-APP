7ec975c8702e97570a6e59570f57d241f3ffdb3a
Faqu22 <facundoalvarez415@gmail.com>
2024-04-15 10:26:17 -0300

    update commands in run.bat

--- run.bat
+++ run.bat
@@ -3,7 +3,24 @@ echo Iniciando servicio del backend
 
 cd backend
 
-run.bat
+docker-compose up -d web
+
+:loop
+for /f %%i in ('docker inspect -f "{{.State.Running}}" backend_frogmi') do (
+    if "%%i"=="true" (
+        goto :next
+    )
+)
+echo Esperando a que el contenedor del backend inicie completamente...
+timeout /t 3 /nobreak >nul
+goto :loop
+
+:next
+echo Iniciando el contenedor de tareas...
+
+docker-compose up -d cron
+
+echo ¡Todos los contenedores del backend han sido iniciados correctamente!
 
 cd ..
 
